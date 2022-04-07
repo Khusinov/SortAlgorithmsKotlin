fc4397d922f2fb374e93eb4e292861ad2bacc527
Khusinov <shaxriyorxusinov2020@gmail.com>
2022-04-08 01:56:26 +0500

    first algorithm

---
+++ src/InsertionSort.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val array = arrayListOf(3, 4, 6, 12, -5, 2, 61, 23)
+
+    println(insertionSort(array))
+}
+
+fun insertionSort(array: ArrayList<Int>): ArrayList<Int> {
+    var pos: Int
+    for (i in 1 until array.size) {
+        val temp = array[i]
+        pos = i
+        while (pos > 0 && temp < array[pos - 1]) {
+            val a = array[pos]
+            array[pos] = array[pos - 1]
+            array[pos - 1] = a
+            pos--
+        }
+    }
+    return array
+}
